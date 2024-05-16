ef06b55e9acc6df48671cf87995d281400d1bc35
muhammad-ali79 <alihelium369@gmail.com>
2024-05-16 06:02:06 +0500

    home page completed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    // optimize images from these domain but deprecated since nextjs14
+    domains: [
+      "uploadthing.com",
+      "utfs.io",
+      "img.clerk.com",
+      "subdomain",
+      "files.stripe.com",
+    ],
+  },
+  reactStrictMode: false,
+};
 
 export default nextConfig;
